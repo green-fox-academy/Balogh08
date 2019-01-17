a4226725b4d6392f09616094e04ccb42a3b39c21
Botond Balogh <botond.balogh8@gmail.com>
2019-01-17 13:50:57 +0100

    Code 'introduceYourself' 'twoNumbers' and 'CodingHours'

---
+++ week-01/day-4/src/CodingHours.java
@@ -0,0 +1,7 @@
+public class CodingHours {
+    public static void main(String[] args) {
+        System.out.println("Hours spent with coding " + (6*17*5));
+        System.out.println("Percentage of coding hours in case of 52 weekly working hours " + ((6*17*5)/(52*17.))*100 + " %");
+
+    }
+}
---
+++ week-01/day-4/src/IntroduceYourself.java
@@ -0,0 +1,7 @@
+public class IntroduceYourself {
+    public static void main(String[] args){
+        System.out.println("Nick Name: Boti");
+        System.out.println("Age: 23");
+        System.out.println("Height: 1.8 m");
+    }
+}
