9392e3ef8a940585a225f226f4d1533c01a16a9a
Botond Balogh <botond.balogh8@gmail.com>
2019-01-17 12:59:51 +0100

    set .gitignore and do the first 3 java tasks

---
+++ week-01/day-4/src/HelloOthers.java
@@ -0,0 +1,7 @@
+public class HelloOthers {
+    public static void main(String[] args) {
+        System.out.println("Hello János!");
+        System.out.println("Hello Anikó!");
+        System.out.println("Hello Krisztián!");
+    }
+}
