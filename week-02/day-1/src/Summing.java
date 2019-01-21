00b117cdfc6ac68f3db5989a6648b4bea633118e
Botond Balogh <botond.balogh8@gmail.com>
2019-01-21 14:00:00 +0100

    Add numbers

---
+++ week-02/day-1/src/Summing.java
@@ -0,0 +1,15 @@
+public class Summing {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 10;
+        int add = 0;
+        add = sum(num1 , num2);
+        System.out.println(add);
+
+    }
+    public static int sum(int a, int b){
+        int c = 0;
+        c = a + b;
+        return c;
+    }
+}
