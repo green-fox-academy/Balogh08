674d2d1aed159e9e260644cdd4021c20ec81caac
Botond Balogh <botond.balogh8@gmail.com>
2019-01-21 15:29:02 +0100

    Increment an element in an array

--- week-02/day-1/src/ChangeElements.java
+++ week-02/day-1/src/ChangeElements.java
@@ -2,6 +2,6 @@ public class ChangeElements {
     public static void main(String[] args) {
         int[] s = {1,2,3,8,5,6};
         s[3] = 4;
-        System.out.println(s[3]);
+        System.out.println(s[3  ]);
     }
 }
