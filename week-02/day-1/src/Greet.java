d3e2a972e06445a9aca17a2e5989d74e30e92678
Botond Balogh <botond.balogh8@gmail.com>
2019-01-21 13:16:41 +0100

    Greets Greanfox

--- week-02/day-1/src/Greet.java
+++ week-02/day-1/src/Greet.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
-
 public class Greet {
+
+    public static void main(String[] args) {
+        String a1 = "Greenfox";
+        greetings(a1);
+    }
+    public static void greetings(String name1){
+        System.out.println("Greetings dear, " + name1);
+        }
+
 }
