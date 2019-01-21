43c22c5cd4913a6b1374bfe5f2df0dfd28e20174
Botond Balogh <botond.balogh8@gmail.com>
2019-01-21 15:03:55 +0100

    Sum x numbers

--- week-02/day-1/src/Summing.java
+++ week-02/day-1/src/Summing.java
@@ -1,15 +1,25 @@
+import java.util.Scanner;
+
 public class Summing {
     public static void main(String[] args) {
-        int num1 = 5;
-        int num2 = 10;
-        int add = 0;
-        add = sum(num1 , num2);
-        System.out.println(add);
+        Scanner num1 = new Scanner(System.in);
 
+        System.out.println("How many numbers should be added?");
+        int userInput1 = num1.nextInt();
+        System.out.println(sum(userInput1));
     }
-    public static int sum(int a, int b){
-        int c = 0;
-        c = a + b;
-        return c;
-    }
+        public static int sum(int userParameter)
+        {
+            int sumOfNumbers = 0;
+            for (int i = 0; i < userParameter; i++) {
+                System.out.println("Then give me " + (userParameter - i) + " number(s)!");
+                Scanner num2 = new Scanner(System.in);
+                int userNumbers = num2.nextInt();
+                sumOfNumbers = sumOfNumbers + userNumbers;
+
+            }
+            return sumOfNumbers;
+        }
 }
+
+
