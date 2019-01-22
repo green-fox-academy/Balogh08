6a393edcf6647702ce02270e71e8c0491e02045e
Botond Balogh <botond.balogh8@gmail.com>
2019-01-22 14:54:34 +0100

    Macth boys and girls from 2 ArrayList

---
+++ week-02/day-2/src/Matchmaking.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Matchmaking {
+    public static void main(String[] args) {
+        ArrayList<String> girls = new ArrayList<String>(Arrays.asList("Eve","Ashley","Claire","Kat","Jane","Rozi","Natália","Móni"));
+        ArrayList<String> boys = new ArrayList<String>(Arrays.asList("Joe","Fred","Tom","Todd","Neef","Jeff"));
+
+        ArrayList<String> pairs = new ArrayList<String>();
+
+        System.out.println(makingMatches(girls,boys,pairs));
+    }
+
+
+    public static Object makingMatches(ArrayList a, ArrayList b, ArrayList c){
+        int maxSize = 0;
+        if (a.size() >= b.size()){
+            maxSize = a.size();
+        }
+        else{
+            maxSize = b.size();
+        }
+
+        for (int i = 0; i < maxSize; i++) {
+            if (i < a.size()){
+                c.add(a.get(i));
+            }
+            if (i < b.size()){
+                c.add(b.get(i));
+            }
+        }
+        return c;
+    }
+}
