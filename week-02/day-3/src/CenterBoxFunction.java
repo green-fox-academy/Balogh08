815eaee4b80f3b3b9f05731e989202be21985cb1
Botond Balogh <botond.balogh8@gmail.com>
2019-01-23 13:24:53 +0100

    Draw 3 squares chich middle point is in the center of the canvas

--- week-02/day-3/src/CenterBoxFunction.java
+++ week-02/day-3/src/CenterBoxFunction.java
@@ -6,7 +6,7 @@ import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
 public class CenterBoxFunction {
 
-    public static void mainDraw(Graphics g){
+    public static void mainDraw(Graphics g) {
         // create a square drawing function that takes 2 parameter:
         // the square size and the graphics
         // and draws a square of that size to the center of the canvas.
@@ -14,7 +14,7 @@ public class CenterBoxFunction {
         // avoid code duplication.
 
 
-        for (int i = 0; i < 3; i++) {
+        /*for (int i = 0; i < 3; i++) {
 
             int rndSize = (int) (Math.random() * 100);
             int rndRGB = (int) (Math.random() * 255);
@@ -22,9 +22,26 @@ public class CenterBoxFunction {
 
             g.setColor(new Color(rndRGB, rndRGB, rndRGB));
             g.drawRect(startPos, startPos, rndSize, rndSize);
+        }*/
+
+        int rndSize = 0;
+        int startPos = 0;
+        int rndRGB = 0;
+
+        drawRectsInTheMiddle(rndSize, startPos, rndRGB, g);
+    }
+
+        public static void drawRectsInTheMiddle(int sizeOfRect, int startOfRect,int colorOfRect, Graphics g){
+            for (int i = 0; i < 3; i++) {
+                sizeOfRect = (int) (Math.random() * 100);
+                startOfRect = 160 - sizeOfRect / 2;
+                colorOfRect = (int) (Math.random() * 255);
+                g.setColor(new Color(colorOfRect, colorOfRect, colorOfRect));
+                g.drawRect(startOfRect, startOfRect, sizeOfRect, sizeOfRect);
+            }
         }
 
-    }
+
 
     // Don't touch the code below
     static int WIDTH = 320;
