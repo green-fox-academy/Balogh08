8a5d2983d4df4a5f84a5891c3a5fc1c1d6e6b57c
Botond Balogh <botond.balogh8@gmail.com>
2019-01-23 17:06:35 +0100

    Draw random stars on the black sky

---
+++ week-02/day-3/src/StarryNight.java
@@ -0,0 +1,48 @@
+import javax.swing.*;
+import javax.swing.plaf.ColorUIResource;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class StarryNight {
+    public static void mainDraw(Graphics g) {
+        // Draw the night sky:
+        //  - The background should be black
+        //  - The stars can be small squares
+        //  - The stars should have random positions on the canvas
+        //  - The stars should have random color (some shade of grey)
+
+        for (int i = 0; i <100; i++) {
+            int randomX = (int)(Math.random()*1920);
+            int randomY = (int)(Math.random()*1080);
+            int colorRGB = (int)(Math.random()*255);
+            g.setColor(new Color(colorRGB,colorRGB,colorRGB));
+            g.fillOval(randomX, randomY, 1, 10);
+        }
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 320;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImagePanel panel = new ImagePanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+            this.setBackground(Color.black);
+        }
+    }
+}
