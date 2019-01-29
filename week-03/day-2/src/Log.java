376306af0869170978a8c50a1c239782da97994e
Botond Balogh <botond.balogh8@gmail.com>
2019-01-29 22:02:27 +0100

    Get unique IP addresses and ratio of POST/GET

---
+++ week-03/day-2/src/Log.java
@@ -0,0 +1,79 @@
+import java.lang.reflect.Array;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static java.util.Arrays.sort;
+
+public class Log {
+    public static void main(String[] args) {
+        Path filePathOfIp = Paths.get("logs.txt");
+        List<String> arrayListOfUniqueIP = new ArrayList<>();
+
+        test1(arrayListOfUniqueIP,filePathOfIp);
+    }
+
+
+public static void test1(List<String> arrayFromGetValues, Path sourceFile){
+        try {
+            arrayFromGetValues.addAll(Files.readAllLines(sourceFile));
+        }catch ( Exception ex){
+            System.out.println("Oh, nooo! Something went wrong!");
+        }
+
+        String[] arrayToStoreInitializedValues = arrayFromGetValues.toArray(new String[0]);
+
+        String[] arrayUniqueIP = new String[arrayFromGetValues.size()];
+        String[] arrayPostGet = new String[arrayFromGetValues.size()];
+        String[] splitArray = new String[arrayFromGetValues.size()];
+
+        for (int i = 0; i < arrayFromGetValues.size() ; i++) {
+            splitMethod(arrayToStoreInitializedValues, splitArray, arrayUniqueIP, arrayPostGet, i);
+        }
+
+        double amountOfGet = 0;
+        double amountOfPost = 0;
+
+        for (int i = 0; i < arrayPostGet.length; i++) {
+            if (arrayPostGet[i].equals("POST /")){
+                amountOfGet += 1;
+            }
+            else{
+                amountOfPost += 1;
+            }
+        }
+
+
+        sort(arrayUniqueIP);
+        ArrayList<String> uniqueIPsArrayList = new ArrayList<>();
+
+        for (int i = 0; i < arrayUniqueIP.length; i++) {
+
+            try{if(!arrayUniqueIP[i].equals(arrayUniqueIP[i + 1])){
+                uniqueIPsArrayList.add(arrayUniqueIP[i]);
+            }}catch (ArrayIndexOutOfBoundsException ex){
+                uniqueIPsArrayList.add(arrayUniqueIP[i]);
+            }
+
+        }
+
+        for (int i = 0; i < uniqueIPsArrayList.size(); i++) {
+            System.out.println(uniqueIPsArrayList.get(i));
+        }
+
+        double ratioOfPostGet = amountOfPost / amountOfGet;
+        double ratioOfGetPost = amountOfGet / amountOfPost;
+        System.out.println("Post / Get = " + ratioOfPostGet);
+        System.out.println("Get / Post = " + ratioOfGetPost);
+    }
+
+    public static void splitMethod(String[] arrayListWhichValuesWillBeSplited, String[] arrayToStoreValues, String[] uniqueIP, String[] arrayPostGet, int index){
+
+        arrayToStoreValues = arrayListWhichValuesWillBeSplited[index].split("  ");
+        uniqueIP[index] = arrayToStoreValues[2].trim();
+        arrayPostGet[index] = arrayToStoreValues[3].trim();
+    }
+}
