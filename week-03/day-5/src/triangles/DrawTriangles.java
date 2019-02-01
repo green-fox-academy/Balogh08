dc045cbb72f940052765175c20a03eef16466ca2
Botond Balogh <botond.balogh8@gmail.com>
2019-02-01 13:02:44 +0100

    Reorganize function to classes

--- week-03/day-5/src/triangles/DrawTriangles.java
+++ week-03/day-5/src/triangles/DrawTriangles.java
@@ -8,63 +8,11 @@ import static javax.swing.JFrame.EXIT_ON_CLOSE;
 public class DrawTriangles {
     public static void mainDraw(Graphics g) {
 
-
-        drawRecursiveEquilateralTriangle(WIDTH / 4, 0, WIDTH / 2, g, 10);
-    }
-
-    public static void drawRecursiveEquilateralTriangle(int x, int y, int width, Graphics g, int level){
-
-        setRandomColors(g);
-
-        drawTriangle(x, y, width, g);
-        if (level == 0) {
-            drawRotatetdTriangle(0, 0, WIDTH,  g);
-        }
-        else {
-            int heightOfRecursiveTriangle = (int) (width/2 * Math.sqrt(3));
-            drawRecursiveEquilateralTriangle(x - (width / 4), y, width / 2, g, level - 1);  //left
-            drawRecursiveEquilateralTriangle(x + ( 3 * (width / 4)), y, width / 2, g, level - 1);  //right
-            drawRecursiveEquilateralTriangle(x + (width / 4), y + heightOfRecursiveTriangle, width / 2,  g, level - 1);  //bottom
-        }
-    }
-
-    public static void drawTriangle(int x, int y, int width, Graphics g) {
-
-        int heightOfTriangle = (int) (width/2 * Math.sqrt(3));
-
-        int[][] coordinatesOfTriangle = new int[2][3];
-        coordinatesOfTriangle[0][0] = x + width/2;
-        coordinatesOfTriangle[1][0] = y;
-        coordinatesOfTriangle[0][1] = x + width;
-        coordinatesOfTriangle[1][1] = y + heightOfTriangle;
-        coordinatesOfTriangle[0][2] = x;
-        coordinatesOfTriangle[1][2] = y + heightOfTriangle;
-
-        g.drawPolygon(coordinatesOfTriangle[0], coordinatesOfTriangle[1], coordinatesOfTriangle[0].length);
-    }
-
-    public static void drawRotatetdTriangle(int x, int y, int width, Graphics g) {
-
-        int heightOfTriangle = (int) (width/2 * Math.sqrt(3));
-
-        int[][] coordinatesOfTriangle = new int[2][3];
-        coordinatesOfTriangle[0][0] = x;
-        coordinatesOfTriangle[1][0] = y;
-        coordinatesOfTriangle[0][1] = x + width;
-        coordinatesOfTriangle[1][1] = y;
-        coordinatesOfTriangle[0][2] = x + width / 2;
-        coordinatesOfTriangle[1][2] = y + heightOfTriangle;
-
-        g.drawPolygon(coordinatesOfTriangle[0], coordinatesOfTriangle[1], coordinatesOfTriangle[0].length);
-    }
-
-    public static void setRandomColors(Graphics g) {
-
-        int redColor = (int) (Math.random() * 255);
-        int greenColor = (int) (Math.random() * 255);
-        int blueColor = (int) (Math.random() * 255);
-
-        g.setColor(new Color(redColor, greenColor, blueColor));
+        //Triangle.drawRecursiveTriangle(WIDTH / 4, 0, WIDTH / 2, g, 10);
+        //Triangle.drawRecursiveTriangle(500, 500, 200, g, 3 );
+        Triangle myTriangle = new Triangle();
+        myTriangle.drawRecursiveTriangle(150, 50, 200, g, 8);
+        myTriangle.drawRotatetdTriangle(150, 50, 200, g);
     }
 
     // Don't touch the code below
@@ -80,7 +28,7 @@ public class DrawTriangles {
         jFrame.setLocationRelativeTo(null);
         jFrame.setVisible(true);
         jFrame.pack();
-        panel.setBackground(Color.BLACK);
+            panel.setBackground(Color.BLACK);
     }
 
     static class ImagePanel extends JPanel {
--- week-03/day-5/src/triangles/Triangle.java
+++ week-03/day-5/src/triangles/Triangle.java
@@ -1,5 +1,82 @@
 package triangles;
 
+import java.awt.*;
+
+import static triangles.DrawTriangles.WIDTH;
+
 public class Triangle {
-    
+
+    int x;
+    int y;
+    int width;
+    Graphics g;
+
+    public Triangle() {
+
+    }
+
+    public Triangle(int x, int y, int width, Graphics g) {
+
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.g = g;
+    }
+
+    public void drawTriangle(){
+
+        int heightOfTriangle = (int) (width/2 * Math.sqrt(3));
+
+        int[][] coordinatesOfTriangle = new int[2][3];
+        coordinatesOfTriangle[0][0] = x + width/2;
+        coordinatesOfTriangle[1][0] = y;
+        coordinatesOfTriangle[0][1] = x + width;
+        coordinatesOfTriangle[1][1] = y + heightOfTriangle;
+        coordinatesOfTriangle[0][2] = x;
+        coordinatesOfTriangle[1][2] = y + heightOfTriangle;
+
+        g.drawPolygon(coordinatesOfTriangle[0], coordinatesOfTriangle[1], coordinatesOfTriangle[0].length);
+    }
+
+    public void drawRotatetdTriangle( int x, int y, int width, Graphics g){
+
+        int heightOfTriangle = (int) (width * Math.sqrt(3));
+        int widhtOfRotatedTriangle = width * 2;
+
+        int[][] coordinatesOfTriangle = new int[2][3];
+        coordinatesOfTriangle[0][0] = x - widhtOfRotatedTriangle / 4;
+        coordinatesOfTriangle[1][0] = y;
+        coordinatesOfTriangle[0][1] = x + widhtOfRotatedTriangle / 4 * 3 ;
+        coordinatesOfTriangle[1][1] = y;
+        coordinatesOfTriangle[0][2] = x + widhtOfRotatedTriangle / 4;
+        coordinatesOfTriangle[1][2] = y + heightOfTriangle;
+
+        g.drawPolygon(coordinatesOfTriangle[0], coordinatesOfTriangle[1], coordinatesOfTriangle[0].length);
+    }
+
+    public  void drawRecursiveTriangle(int x, int y, int width, Graphics g, int level) {
+        Triangle myTriangle = new Triangle(x, y, width, g);
+        myTriangle.setRandomColors(g);
+        myTriangle.drawTriangle();
+
+        if (level == 0) {
+        }
+        else {
+            int heightOfRecursiveTriangle = (int) (width/2 * Math.sqrt(3));
+            drawRecursiveTriangle(x - (width / 4), y, width / 2, g, level - 1);  //left
+            drawRecursiveTriangle(x + ( 3 * (width / 4)), y, width / 2, g, level - 1);  //right
+            drawRecursiveTriangle(x + (width / 4), y + heightOfRecursiveTriangle, width / 2,  g, level - 1);  //bottom
+        }
+
+    }
+
+    public void setRandomColors(Graphics g) {
+
+        int redColor = (int) (Math.random() * 255);
+        int greenColor = (int) (Math.random() * 255);
+        int blueColor = (int) (Math.random() * 255);
+
+        g.setColor(new Color(redColor, greenColor, blueColor));
+    }
+
 }
