338173f3f2afe6f42b751caae1d63c14354aa0cd
Botond Balogh <botond.balogh8@gmail.com>
2019-02-06 12:54:18 +0100

    Sort dominos in ascending order

---
+++ week-04/day-3/src/comparable/Domino.java
@@ -0,0 +1,38 @@
+package comparable;
+
+public class Domino implements Comparable<Domino>{
+    private  int left;
+    private  int right;
+
+    public Domino(int left, int right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    public int getLeftSide() {
+        return left;
+    }
+
+    public int getRightSide() {
+        return right;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + left + ", " + right + "]";
+    }
+
+    public int compareTo(Domino domino){
+        if(this.getLeftSide() < domino.getLeftSide()) {
+            return -1;
+        } else if (this.getLeftSide() > domino.getLeftSide()) {
+            return 1;
+        } else if (this.getRightSide() < domino.getRightSide()) {
+            return -1;
+        } else if (this.getRightSide() > domino.getRightSide()) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+}
