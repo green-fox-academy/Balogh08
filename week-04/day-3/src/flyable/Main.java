9b6f9552a40d007ef9d810c5ac3570503ab13866
Botond Balogh <botond.balogh8@gmail.com>
2019-02-10 17:58:24 +0100

    Add bird to main

--- week-04/day-3/src/flyable/Main.java
+++ week-04/day-3/src/flyable/Main.java
@@ -1,11 +1,16 @@
 package flyable;
 
+import animal.Bird;
+
 public class Main {
     public static void main(String[] args) {
         Vehicle myHelicopter = new Helicopter("Tesla", "black" , 130);
         ((Helicopter)myHelicopter).fly();
         ((Helicopter) myHelicopter).land();
         ((Helicopter) myHelicopter).takeOff();
+
+        Bird myBird = new Bird("madár", 5);
+        myBird.takeOff();
 
     }
 
