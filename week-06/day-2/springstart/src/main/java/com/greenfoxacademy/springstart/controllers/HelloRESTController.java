f0f23a6e19f16707e99411fb2cadafd326f9c46a
Botond Balogh <botond.balogh8@gmail.com>
2019-02-26 12:20:31 +0100

    Get name

--- week-06/day-2/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
+++ week-06/day-2/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
@@ -1,14 +1,15 @@
 package com.greenfoxacademy.springstart.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloRESTController {
 
     @RequestMapping("/greeting")
-    public static Greeting Greeting() {
-        Greeting greeting = new Greeting(101, "Hello Boti!");
+    public static Greeting Greeting(@RequestParam String name) {
+        Greeting greeting = new Greeting(101, "Hello " + name + "!");
         return greeting;
     }
 }
