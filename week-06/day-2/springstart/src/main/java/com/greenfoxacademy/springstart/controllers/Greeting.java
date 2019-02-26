98f1f8859aa6c3b3b0c6dd9d6d4d0ac0c21b3df7
Botond Balogh <botond.balogh8@gmail.com>
2019-02-26 17:04:19 +0100

    Greet in random languge with random font-size and color

--- week-06/day-2/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/Greeting.java
+++ week-06/day-2/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/Greeting.java
@@ -6,10 +6,27 @@ public class Greeting {
     private long id;
     private String content;
     private static AtomicLong counter = new AtomicLong();
+    private String greet;
+    private int size;
+    private int redColor;
+    private int greenColor;
+    private int blueColor;
+    private static int randomIndex;
 
-    public Greeting( String content) {
+    private String[] hellos = {"Mirëdita", "Ahalan", "Parev", "Zdravei", "Nei Ho", "Dobrý den", "Ahoj", "Goddag", "Goede dag, Hallo", "Hello", "Saluton", "Hei", "Bonjour",
+            "Guten Tag", "Gia'sou", "Aloha", "Shalom", "Namaste", "Namaste", "Jó napot", "Halló", "Helló", "Góðan daginn", "Halo", "Aksunai", "Qanuipit", "Dia dhuit",
+            "Salve", "Ciao", "Kon-nichiwa", "An-nyong Ha-se-yo", "Salvëte", "Ni hao", "Dzien' dobry", "Olá", "Bunã ziua", "Zdravstvuyte", "Hola", "Jambo", "Hujambo", "Hej",
+            "Sa-wat-dee", "Merhaba", "Selam", "Vitayu", "Xin chào", "Hylo", "Sut Mae", "Sholem Aleychem", "Sawubona"};
+
+    public Greeting(String content) {
         id = counter.incrementAndGet();
         this.content = content;
+        size = (int)(Math.random() * 20 + 6);
+        redColor = (int)(Math.random() * 255);
+        greenColor = (int)(Math.random() * 255);
+        blueColor = (int)(Math.random() * 255);
+        randomIndex= (int)(Math.random() * hellos.length);
+        greet = hellos[randomIndex];
     }
 
     public long getId() {
@@ -18,5 +35,25 @@ public class Greeting {
 
     public String getContent() {
         return content;
+    }
+
+    public String getGreet() {
+        return greet;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public int getRedColor() {
+        return redColor;
+    }
+
+    public int getGreenColor() {
+        return greenColor;
+    }
+
+    public int getBlueColor() {
+        return blueColor;
     }
 }
--- week-06/day-2/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
+++ week-06/day-2/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
@@ -14,6 +14,22 @@ public class HelloWebController {
         Greeting greeting = new Greeting(name);
         model.addAttribute("name", greeting.getContent());
         model.addAttribute("id", greeting.getId());
+        model.addAttribute("greet", greeting.getGreet());
+        model.addAttribute("size", greeting.getSize());
+        model.addAttribute("red", greeting.getRedColor());
+        model.addAttribute("green", greeting.getGreenColor());
+        model.addAttribute("blue", greeting.getBlueColor());
         return "greeting";
+    }
+
+    @RequestMapping("/web/languages")
+    public String languages(Model model) {
+        Greeting greeting = new Greeting("Boti");
+        model.addAttribute("greet", greeting.getGreet());
+        model.addAttribute("size", greeting.getSize());
+        model.addAttribute("red", greeting.getRedColor());
+        model.addAttribute( "green", greeting.getGreenColor());
+        model.addAttribute("blue", greeting.getBlueColor());
+        return "languages";
     }
 }
