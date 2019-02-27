7c543402904a8cbfd6ba95f9dc0c7e6b63006de1
Botond Balogh <botond.balogh8@gmail.com>
2019-02-27 16:19:00 +0100

    Login page

---
+++ week-06/day-3/login/function.js
@@ -0,0 +1,8 @@
+function mouseoverPass() {
+    var obj = document.getElementById('password');
+    obj.type = "text";
+}
+function mouseoutPass() {
+    var obj = document.getElementById('password');
+    obj.type = "password";
+}
