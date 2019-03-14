3a55071870782eebb7b971197716e6ab25d36edd
Botond Balogh <botond.balogh8@gmail.com>
2019-03-14 11:12:53 +0100

    Create GetMapping for main page

--- week-08/day-4/src/main/java/com/greenfoxacademy/reddit/controller/RedditController.java
+++ week-08/day-4/src/main/java/com/greenfoxacademy/reddit/controller/RedditController.java
@@ -1,7 +1,26 @@
 package com.greenfoxacademy.reddit.controller;
 
+import com.greenfoxacademy.reddit.service.RedditService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping("/")
 public class RedditController {
+
+    private RedditService redditService;
+
+    @Autowired
+    public RedditController(RedditService redditService) {
+        this.redditService = redditService;
+    }
+
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public String main(Model model) {
+        model.addAttribute("posts", redditService.listAll());
+        return "main";
+    }
 }
