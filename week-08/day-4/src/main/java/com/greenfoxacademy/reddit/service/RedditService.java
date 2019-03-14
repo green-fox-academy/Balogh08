86a653776bdd4b36d3cc4f4bc83a9d279024f609
Botond Balogh <botond.balogh8@gmail.com>
2019-03-14 11:09:16 +0100

    List all post

--- week-08/day-4/src/main/java/com/greenfoxacademy/reddit/service/RedditService.java
+++ week-08/day-4/src/main/java/com/greenfoxacademy/reddit/service/RedditService.java
@@ -1,7 +1,25 @@
 package com.greenfoxacademy.reddit.service;
 
+import com.greenfoxacademy.reddit.model.Post;
+import com.greenfoxacademy.reddit.repository.RedditRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class RedditService {
+    private RedditRepository redditRepository;
+
+    @Autowired
+    public RedditService(RedditRepository redditRepository) {
+        this.redditRepository = redditRepository;
+    }
+
+    public List<Post> listAll() {
+        List<Post> result = new ArrayList<>();
+        redditRepository.findAll().forEach(result::add);
+        return result;
+    }
 }
