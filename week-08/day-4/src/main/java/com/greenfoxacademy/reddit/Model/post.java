dbfaa0783addc9be86b302a3b99555824d263ed3
Botond Balogh <botond.balogh8@gmail.com>
2019-03-14 10:59:52 +0100

    Create constructor and getter/setter

--- week-08/day-4/src/main/java/com/greenfoxacademy/reddit/Model/post.java
+++ week-08/day-4/src/main/java/com/greenfoxacademy/reddit/Model/post.java
@@ -13,6 +13,25 @@ public class post {
     private String title;
 
     public post() {
+    }
 
+    public post(String title) {
+        this.title = title;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
     }
 }
