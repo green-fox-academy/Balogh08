b668a9ba830badf85670f20a42f47fe28013c5f4
Botond Balogh <botond.balogh8@gmail.com>
2019-03-16 22:13:30 +0100

    Add 'back to todo list' button

--- week-08/day-5/todo_practice/src/main/java/com/greenfoxacademy/practicetodo/controller/AssigneeController.java
+++ week-08/day-5/todo_practice/src/main/java/com/greenfoxacademy/practicetodo/controller/AssigneeController.java
@@ -4,10 +4,7 @@ import com.greenfoxacademy.practicetodo.service.AssigneeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/todo")
