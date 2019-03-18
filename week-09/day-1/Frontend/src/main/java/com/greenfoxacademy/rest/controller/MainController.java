864b3fcfd5a7382554f7dccce4b0a8ae9c7f1f61
Botond Balogh <botond.balogh8@gmail.com>
2019-03-18 14:27:23 +0100

    Greet student

--- week-09/day-1/Frontend/src/main/java/com/greenfoxacademy/rest/controller/MainController.java
+++ week-09/day-1/Frontend/src/main/java/com/greenfoxacademy/rest/controller/MainController.java
@@ -2,10 +2,10 @@ package com.greenfoxacademy.rest.controller;
 
 import com.greenfoxacademy.rest.model.Doubling;
 import com.greenfoxacademy.rest.model.ErrorMessage;
+import com.greenfoxacademy.rest.model.Greeter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
@@ -13,16 +13,5 @@ public class MainController {
     @GetMapping("/")
     public String index() {
         return "index";
-    }
-
-    @GetMapping("/doubling")
-    @ResponseBody
-    public Object doubling(@RequestParam(value = "input", required = false) Integer input) {
-        if (input != null) {
-            return new Doubling(input);
-        } else {
-            return new ErrorMessage("Please provide an input");
-        }
-
     }
 }
---
+++ week-09/day-1/Frontend/src/main/java/com/greenfoxacademy/rest/controller/RestController.java
@@ -0,0 +1,34 @@
+package com.greenfoxacademy.rest.controller;
+
+import com.greenfoxacademy.rest.model.Doubling;
+import com.greenfoxacademy.rest.model.ErrorMessage;
+import com.greenfoxacademy.rest.model.Greeter;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@org.springframework.web.bind.annotation.RestController
+public class RestController {
+
+    @GetMapping("/doubling")
+    public Object doubling(@RequestParam(value = "input", required = false) Integer input) {
+        if (input != null) {
+            return new Doubling(input);
+        } else {
+            return new ErrorMessage("Please provide an input");
+        }
+    }
+
+
+    @GetMapping("/greeter")
+    public Object greeter(@RequestParam(value = "name", required = false) String name, @RequestParam(value = "title", required = false) String title) {
+        if (name != null && title != null) {
+            return new Greeter(name, title);
+        } else if (name != null) {
+            return new ErrorMessage("Please provide a title!");
+        } else if (title != null) {
+            return new ErrorMessage("Please provide a name!");
+        } else {
+            return new ErrorMessage("Please provide a name and a title!");
+        }
+    }
+}
