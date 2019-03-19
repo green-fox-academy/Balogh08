5499b00b9ddf6bb92c6c73cf6f240af7e9b13b16
Botond Balogh <botond.balogh8@gmail.com>
2019-03-19 11:35:13 +0100

    Pratice mat review

---
+++ week-09/day-2/material_resources/spring_rest_test/src/main/java/com/greenfoxacademy/testing_web/hello/HomeController.java
@@ -0,0 +1,15 @@
+package com.greenfoxacademy.testing_web.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public @ResponseBody String greeting() {
+        return "Hello World";
+    }
+
+}
