26fb8c4ca9e3ceb4bd9aaca537e807000eed7bea
Botond Balogh <botond.balogh8@gmail.com>
2019-04-02 21:53:27 +0200

    Functions

--- week-10/day-2/Practice/FunctionsAndArrays/FunctionsAndArrays/Program.cs
+++ week-10/day-2/Practice/FunctionsAndArrays/FunctionsAndArrays/Program.cs
@@ -10,6 +10,27 @@ namespace FunctionsAndArrays
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Tell me which number to fract!");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine(factorial(num));
+            Console.ReadKey();
+
+            DateTime time = DateTime.Now;
+            Console.WriteLine("Today is {0:d} at {0:T}.", time);
+            Console.WriteLine("Today is {0}.", time);
+            Console.ReadKey();
+        }
+
+        public static int factorial(int num)
+        {
+            int result = 1;
+
+            for (int i = 0; i < num; i++)
+            {
+                result *= (i + 1);
+            }
+
+            return result;
         }
     }
 }
