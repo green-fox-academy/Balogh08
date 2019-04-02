ae5b209f1ff168c3ee429f7e5fcfca39ba0acb64
Botond Balogh <botond.balogh8@gmail.com>
2019-04-02 20:53:06 +0200

    Create new class

---
+++ week-10/day-2/Practice/FunctionsAndArrays/FunctionsAndArrays/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionsAndArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
