baf256f3a53ac65ae171fe2ccda1750e7743b776
Botond Balogh <botond.balogh8@gmail.com>
2019-04-03 14:12:13 +0200

    Read and write files

---
+++ week-10/day-3/ReadFile/ReadFile/Program.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadFile
+{
+    class Program
+    {
+        private static object error;
+
+        static void Main(string[] args)
+        {
+            //The code provided will pgit arint ‘Hello World’ to the console.
+            // Press Ctrl+F5(or go to Debug > Start Without Debugging) to run your app.
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+
+            //Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+
+            File.WriteAllText("myFile", "Hello");
+            Console.WriteLine(File.ReadAllText("myFile"));
+            Console.ReadLine();
+
+            StreamReader myReader = new StreamReader("TextFile1.txt");
+            string line = "";
+
+            while (line != null)
+            {
+                line = myReader.ReadLine();
+                if (line != null)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+
+            Console.WriteLine(File.ReadAllText("TextFile1.txt"));
+            myReader.Close();
+            Console.ReadLine();
+
+            string[] lines = { "This is line1", "This is line2", "This is line5" };
+            System.IO.StreamWriter writer = new System.IO.StreamWriter(@"C:\Users\Balogh Botond\Desktop\GreenFox\Balogh08\week-10\day-3\ReadFile\ReadFile\bin\Debug\myFile.txt");
+
+            using (writer)
+            {
+                foreach (string row in lines)
+                {
+                    writer.WriteLine(row);
+                }
+            }
+
+            int x = 1;
+            int y = 0;
+
+            try
+            {
+                double z = x / y;
+                Console.WriteLine(z);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Cannot divide by 0");
+            }
+            finally
+            {
+                Console.WriteLine(x);
+                Console.WriteLine(y);
+            }
+
+            Console.ReadLine();
+
+            StreamReader myReader2 = new StreamReader(@"..\..\beep.txt");
+
+            string data = myReader2.ReadLine();
+            
+            while (data != null)
+            {
+                Console.WriteLine(data);
+                string[] values = data.Split(',');
+                int frequency = Int32.Parse(values[0]);
+                int duration = Int32.Parse(values[1]);
+                Console.Beep(frequency, duration);
+                data = myReader2.ReadLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
