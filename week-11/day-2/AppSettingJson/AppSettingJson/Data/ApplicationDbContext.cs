8d41932f533d83b364e67c4ba80a23d305861ab9
Botond Balogh <botond.balogh8@gmail.com>
2019-04-09 17:14:37 +0200

    Project setup

---
+++ week-11/day-2/AppSettingJson/AppSettingJson/Data/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿//using Microsoft.EntityFrameworkCore;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+
+//namespace AppSettingJson.Data
+//{
+//    public class ApplicationDbContext : DbContext
+//    {
+//        #region Public Properties
+
+//        public DbSet<SettingsDataModel> Settings { get; set; }
+
+//        #endregion
+
+//        public ApplicationDbContext()
+//        {
+
+//        }
+
+//        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//        {
+//            base.OnConfiguring(optionsBuilder);
+//        }
+//    }
+//}
---
+++ week-11/day-2/AppSettingJson/AppSettingJson/Data/SettingsDataModel.cs
@@ -0,0 +1,9 @@
+﻿//namespace AppSettingJson.Data
+//{
+//    public class SettingsDataModel
+//    {
+//        public string Id { get; set; }
+//        public string Name { get; set; }
+//        public string Value { get; set; }
+//    }
+//}
